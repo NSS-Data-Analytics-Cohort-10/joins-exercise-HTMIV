df0f9f0ea935eb653656c46a18754622df70553f
TomMaster <hercules_masteriv@hotmail.com>
2023-10-07 14:17:48 -0500

    joins final

--- Scripts/joins.sql
+++ Scripts/joins.sql
@@ -120,8 +120,122 @@ SELECT * FROM RATING LIMIT 100;
 SELECT * FROM REVENUE LIMIT 100;
 SELECT * FROM DISTRIBUTORS LIMIT 100;
 */
-
+SELECT 
+	UPPER(D.company_name) distributor_company_name,
+	AVG(R.film_budget) avg_budget
+FROM 
+	distributors D
+	INNER JOIN specs S ON D.distributor_id = S.domestic_distributor_id 
+	INNER JOIN revenue R ON S.movie_id = R.movie_id 
+GROUP BY
+	UPPER(D.company_name)
+ORDER BY 
+	2 DESC
+LIMIT(5)
 
 -- 6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
+/*
+SELECT * FROM SPECS LIMIT 100;
+SELECT * FROM RATING LIMIT 100;
+SELECT * FROM REVENUE LIMIT 100;
+SELECT * FROM DISTRIBUTORS LIMIT 100;
+*/
+
+/*
+How many movies in the dataset are distributed by a company which is not headquartered in California?
+*/
+SELECT SUM(COUNTS) 
+FROM 
+(SELECT
+    UPPER(S.film_title) film_title
+    count(distinct(S.movie_id)) COUNTS,
+    
+FROM
+    Specs S
+	LEFT OUTER JOIN distributors D ON D.distributor_id = S.domestic_distributor_id 
+	LEFT OUTER JOIN rating R ON S.movie_id = R.movie_id
+WHERE 
+	UPPER(SUBSTRING(TRIM(d.headquarters),STRPOS(TRIM(d.headquarters),',')+2)) <>'CA'
+GROUP BY
+	 UPPER(S.film_title)
+)
+
+-- 2
+
+/*
+Which of these movies has the highest imdb rating?
+*/
+
+SELECT
+    UPPER(S.film_title),
+	COALESCE(R.imdb_rating,0) imdb_rating
+FROM
+    Specs S
+	LEFT OUTER JOIN distributors D ON D.distributor_id = S.domestic_distributor_id 
+	LEFT OUTER JOIN rating R ON S.movie_id = R.movie_id
+WHERE 
+	UPPER(SUBSTRING(TRIM(d.headquarters),STRPOS(TRIM(d.headquarters),',')+2)) <>'CA'
+AND imdb_rating =
+(SELECT MAX(imdb_rating)
+FROM(
+
+SELECT
+    UPPER(S.film_title),
+	UPPER(COALESCE(d.headquarters,'*UNASSIGNED')) headquarters,
+	COALESCE(R.imdb_rating,0) imdb_rating
+FROM
+    Specs S
+	LEFT OUTER JOIN distributors D ON D.distributor_id = S.domestic_distributor_id 
+	LEFT OUTER JOIN rating R ON S.movie_id = R.movie_id
+WHERE 
+UPPER(SUBSTRING(TRIM(d.headquarters),STRPOS(TRIM(d.headquarters),',')+2)) <>'CA'))
+
+-- DIRTY DANCING (7.0)
 
 -- 7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
+/*
+SELECT * FROM SPECS LIMIT 100;
+SELECT * FROM RATING LIMIT 100;
+SELECT * FROM REVENUE LIMIT 100;
+SELECT * FROM DISTRIBUTORS LIMIT 100;
+*/
+
+SELECT 
+	HOURS,
+	ROUND(avg_rating,2)
+FROM 
+(SELECT
+	CASE 
+	WHEN S.length_in_min > 120 THEN 'OVER TWO HOURS'
+	WHEN S.length_in_min <= 120 THEN 'UNDER TWO HOURS'
+	END HOURS,
+	AVG(R.imdb_rating) avg_rating
+FROM
+    Specs S
+	INNER JOIN rating R ON S.movie_id = R.movie_id
+GROUP BY
+	CASE 
+	WHEN S.length_in_min > 120 THEN 'OVER TWO HOURS'
+	WHEN S.length_in_min <= 120 THEN 'UNDER TWO HOURS'
+	END)
+	WHERE avg_rating =
+(SELECT 
+	MAX(avg_rating) 
+FROM (
+SELECT
+	CASE 
+	WHEN S.length_in_min > 120 THEN 'OVER TWO HOURS'
+	WHEN S.length_in_min <= 120 THEN 'UNDER TWO HOURS'
+	END HOURS,
+AVG(R.imdb_rating) avg_rating
+FROM
+    Specs S
+	INNER JOIN rating R ON S.movie_id = R.movie_id
+GROUP BY
+	CASE 
+	WHEN S.length_in_min > 120 THEN 'OVER TWO HOURS'
+	WHEN S.length_in_min <= 120 THEN 'UNDER TWO HOURS'
+	END))
+	
+-- BEST RATING: OVER TWO HOURS (7.26)
+
